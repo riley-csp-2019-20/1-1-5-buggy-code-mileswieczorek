550cca39f07e8c94bbc57366aa237fffef8136aa
mileswieczorek <55498077+mileswieczorek@users.noreply.github.com>
2019-09-19 10:18:06 -0400

    Version 2

--- 115MilesWieczorek.py
+++ 115MilesWieczorek.py
@@ -8,16 +8,24 @@ spider.circle(20)
 # configure spider legs
 legs = 8
 length_legs = 80
-spider_angle = 270 / legs  
+spider_angle = 360 / legs  
 spider.pensize(5)
 legs_loop = 0
 # draw spider legs
 while (legs_loop < legs):
   spider.goto(0,20)
-  spider.setheading(spider_angle*legs_loop - 180)
-  spider.setheading(spider_angle*legs_loop + 180)
+  spider.setheading(spider_angle*legs_loop)
   spider.forward(length_legs)
   legs_loop = legs_loop + 1
+spider.up()
+spider.goto (0,60)
+spider.down()
+spider.begin_fill()
+spider.circle(20)
+spider.end_fill()
+
+
+
 spider.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
